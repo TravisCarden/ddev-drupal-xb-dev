019fb4edf667760a91c7084ffea794992e2c2d32
Travis Carden <travis.carden@gmail.com>
2025-01-29 00:29:56 -0500

    Improve self test script.

--- tests/test.sh
+++ tests/test.sh
@@ -14,14 +14,15 @@
 #   cd ddev-drupal-xb-dev
 #   ./tests/test.sh
 
-cd "$(dirname "$0")/../var" && clear || exit 1
+PROJECT_NAME="ddev-drupal-xb-dev"
+PROJECT_DIR="test-site"
+DRUPAL_VERSION="10.x@dev"
+
+cd "$(dirname "$0")/../var/$($PROJECT_DIR)" && clear || exit 1
 clear; set -ev
 
-PROJECT_DIR="test"
-PROJECT_NAME=drupal-xb-dev-test
-
 # Clean up from previous runs.
-PROJECT_EXISTS=$(ddev list | grep -q $PROJECT_NAME) || true
+PROJECT_EXISTS=$(ddev list | grep -q "$PROJECT_NAME.ddev.site") || true
 if [ -d $PROJECT_DIR ] || [ -n "$PROJECT_EXISTS" ]; then
   yes | ddev clean $PROJECT_NAME || true
   ddev remove --unlist $PROJECT_NAME || true
@@ -42,26 +43,32 @@ ddev config \
 ddev config --project-type=drupal --php-version=8.3 --docroot=web
 
 # Create the Drupal project.
-ddev composer create drupal/recommended-project:^11.x-dev --no-install
+ddev composer create drupal/recommended-project:$DRUPAL_VERSION --no-install
 
 # Get the add-on.
-ddev get ../../
+ddev add-on get ../../
 
 # Perform one-time setup operations.
 ddev xb-setup
 
 # Test an update.
-ddev get ../../
+ddev add-on get ../../
 
 # Try to perform one-time setup operations when they've already been done.
 ddev xb-setup
 
 # Make sure all the commands get installed.
 DDEV_HELP_OUTPUT=$(ddev help)
-for COMMAND_NAME in $(find "$(dirname "$0")/../commands" -name 'xb-*' -exec basename {} \;); do
+find "commands" -name 'xb-*' -exec basename {} \; | while read -r COMMAND_NAME; do
   if ! echo "$DDEV_HELP_OUTPUT" | grep -q "$COMMAND_NAME"; then
     echo "Error: $COMMAND_NAME is not installed."
     exit 1
   fi
 done
 echo "All commands are installed."
+
+# Install dev extras.
+ddev xb-dev-extras
+
+# Configure environment for Workspaces development.
+ddev xb-workspaces-dev
